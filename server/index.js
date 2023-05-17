09b01786e1c7ab8dd75475490f548e58e1c4c313
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2023-05-17 19:01:10 +0530

    renamed:    server/server.js -> server/index.js 	modified:   server/package.json

--- server/index.js
+++ server/index.js
@@ -11,10 +11,8 @@ app.use(cors())
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
 
-mongoose.connect('mongodb://127.0.0.1:27017/crud', {useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => console.log("Connected to MongoDB"))
+mongoose.connect('mongodb+srv://maaz:7829212918@users.js6hvo3.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log("Connected to MongoDB and listening to server."))
 .catch((error) => console.log(error))
 
 app.use('/users', usersRoute)
-
-app.listen(5000, () => console.log('Server listening on port 5000'))
