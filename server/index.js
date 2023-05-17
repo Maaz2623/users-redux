1bb6031d6e94d77998c9a96c2e25b270696cc7d1
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2023-05-17 19:06:18 +0530

    modified:   server/index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,9 @@ app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
 
 mongoose.connect('mongodb+srv://maaz:7829212918@users.js6hvo3.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => console.log("Connected to MongoDB and listening to server."))
+.then(() => res.send("Connected to MongoDB and listening to server."))
 .catch((error) => console.log(error))
 
 app.use('/users', usersRoute)
+
+app.listen(5000)
