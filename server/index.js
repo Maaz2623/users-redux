47a60718c23a4d3b8ea7967fc052eac549b5941f
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2023-05-17 19:11:41 +0530

    modified:   .gitignore 	modified:   server/index.js

--- server/index.js
+++ server/index.js
@@ -12,9 +12,9 @@ app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
 
 mongoose.connect('mongodb+srv://maaz:7829212918@users.js6hvo3.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => res.send("Connected to MongoDB and listening to server."))
+.then(() => console.log("Connected to MongoDB and listening to server."))
 .catch((error) => console.log(error))
 
 app.use('/users', usersRoute)
 
-app.listen(5000)
+app.listen(5000, () => console.log("Server listening on port 5000"))
